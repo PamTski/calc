332678c555c53b8de9e33445b27e1cfe456dae76
Peely <philipmartinantolihao@gmail.com>
2025-02-08 15:33:00 +0800

    float changes

--- calc.c
+++ calc.c
@@ -1,41 +1,42 @@
 #include <stdio.h>
 
 
-int adder(int x, int y){
+float adder(float x, float y){
   return x +y;
 }
 
-int subtract(int x, int y){
+float subtract(float x, float y){
   return x - y;
 }
 
-int divide(int x, int y){
+float divide(float x, float y){
   return x / y;
 }
 
-int multiply(int x, int y){
+float multiply(float x, float y){
   return x * y;
 }
 int main(void){
-  int x, y, nChoice;
+  float x, y;
+  int nChoice;
   printf("Enter first operand here: ");
-  scanf("%d", &x);
+  scanf("%f", &x);
   printf("Enter second operand here: ");
-  scanf("%d", &y);
+  scanf("%f", &y);
   printf("1.) Add\n2.)Subtract\n3.) Divide\n4.) Multiply \n");
   scanf("%d", &nChoice);
   switch(nChoice){
     case 1:
-      printf("x + y = %d", adder(x,y));
+      printf("x + y = %.2f", adder(x,y));
       break;
     case 2:
-      printf("x - y = %d", subtract(x, y));
+      printf("x - y = %.2f", subtract(x, y));
       break;
     case 3:
-      printf("x / y = %d", divide(x,y));
+      printf("x / y = %.2f", divide(x,y));
       break;
     case 4:
-      printf("x * y = %d", multiply(x, y));
+      printf("x * y = %.2f", multiply(x, y));
       break;
     default:
       printf("Error occured");
